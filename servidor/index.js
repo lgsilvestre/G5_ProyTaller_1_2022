6eb9eabbe7c2b6ec71905ebae65b47e676f5cb30
ChristianLizama <ckill661@gmail.com>
2022-04-03 21:52:35 -0400

    Primer commit

---
+++ servidor/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.set('port',process.env.port || 3000)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(app.get('port'), () => {
+  console.log('server on port' + app.get('port'));
+})
