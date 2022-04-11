9d22b5e697455db6a7f3a741dff1d0cdfc44984c
Emilio Silva <esilva@tibox.cl>
2022-04-11 01:25:11 -0400

    Hotfix: gitignore .env.prod y env actualizado

--- servidor/mongodbConfig.js
+++ servidor/mongodbConfig.js
@@ -2,7 +2,7 @@
 import mongoose from 'mongoose';
 import dotnev from 'dotenv';
 dotnev.config();
-const uri = `mongodb+srv://api-mongo-kobal:${process.env.mongoPass}@cluster-api.hdybb.mongodb.net/dbAdogtame?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://api-mongo-kobal:${process.env.MONGO_PASS}@cluster-api.hdybb.mongodb.net/dbAdogtame?retryWrites=true&w=majority`;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
