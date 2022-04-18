21ae5f28c1a3bfcb4d1ce84dfe3383dbadb28b63
Marcelo <97marcelo.mg@gmail.com>
2022-04-18 00:18:55 -0400

    Seccion Aportes y Eventos, modificacion Nosotros.

--- cliente/src/router/index.js
+++ cliente/src/router/index.js
@@ -20,6 +20,22 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
+    path: '/eventos',
+    name: 'Eventos',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "eventos" */ '../views/Eventos.vue')
+  },
+  {
+    path: '/aportes',
+    name: 'Aportes',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "aportes" */ '../views/Aportes.vue')
+  },
+  {
     path: '/login',
     name: 'Login',
     // route level code-splitting
