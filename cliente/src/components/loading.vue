5555453fb7189cd081ff6bf1a08be1258f004283
ChristianLizama <ckill661@gmail.com>
2022-04-24 23:22:03 -0400

    loading en adoptame

--- cliente/src/components/loading.vue
+++ cliente/src/components/loading.vue
@@ -1,16 +1,14 @@
-<template>
+<template> 
     <div class="box">
         <v-img 
-            src="https://i.pinimg.com/originals/68/17/dc/6817dccec8fb261cc4adc3ab6fc52cfc.gif" 
-            max-height="800"         
-            max-width="800"
+        src="https://i.pinimg.com/originals/68/17/dc/6817dccec8fb261cc4adc3ab6fc52cfc.gif" 
         ></v-img>
-        <!-- <v-progress-circular
-            indeterminate
-            color="red lighten-2"
-            :size="50"
-        ></v-progress-circular> -->
     </div>
+    <!-- <v-progress-circular
+        indeterminate
+        color="red lighten-2"
+        :size="50"
+    ></v-progress-circular> -->
 </template>
 
 <script>
@@ -22,18 +20,9 @@ export default {
 </script>
 
 <style scoped>
-.loading{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    margin: -25px 0 0 -25px;
-}
 .box {
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: column;
-  align-items: center;
-  align-content: space-between;
+  margin: 0 auto;
+  max-width: 80%;
 }
 .v-progress-circular {
   margin: 1rem;
