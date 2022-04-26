00f502a8759779588d0d0adae4dee692f06d2ef8
Marcelo <97marcelo.mg@gmail.com>
2022-04-26 14:33:00 -0400

    Avance preliminar aportes

--- cliente/src/views/Aportes.vue
+++ cliente/src/views/Aportes.vue
@@ -1,15 +1,15 @@
 <template id="aportes">
   <v-layout row>
     <v-flex xs12 sm6 offset-sm3>
-      <v-card>
+      <v-card style="margin-top: 20px" >
         <v-img
           src="https://2.bp.blogspot.com/-DqHwkD4_FQ8/T4ycFeIr0PI/AAAAAAAAIw8/NeoSIEdYcDs/s1600/PAISAJES+CON+PERROS+(2).jpg"
           height="400px"
         >
-        <div div class="text-xs-center" >
+        <div div class="text-xs-center"  >
             
             <h1 class="red white--text" >Donaciones
-              
+      
             </h1>    
             
             <p class="white--text">Cuenta Rut - Banco estado 
@@ -30,6 +30,58 @@
         
       </v-card>
     </v-flex>
+
+
+    <v-card
+    class="mx-auto"
+    max-width="344"
+    style="margin-top: 100px; margin-bottom: 20px"
+    >
+    <v-card-text >
+      <div>Word of the Day</div>
+      <p class="text-h4 text--primary">
+        el·ee·mos·y·nar·y
+      </p>
+      <p>adjective</p>
+      <div class="text--primary">
+        relating to or dependent on charity; charitable.<br>
+        "an eleemosynary educational institution."
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        text
+        color="teal accent-4"
+        @click="reveal = true"
+      >
+        Learn More
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <v-card
+        v-if="reveal"
+        class="transition-fast-in-fast-out v-card--reveal"
+        style="height: 100%;"
+      >
+        <v-card-text class="pb-0">
+          <p class="text-h4 text--primary">
+            Origin
+          </p>
+          <p>late 16th century (as a noun denoting a place where alms were distributed): from medieval Latin eleemosynarius, from late Latin eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’ </p>
+        </v-card-text>
+        <v-card-actions class="pt-0">
+          <v-btn
+            text
+            color="teal accent-4"
+            @click="reveal = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-expand-transition>
+  </v-card>
   </v-layout>  
 
 </template>
@@ -42,6 +94,18 @@ height:100%;			/* El tamaño se establece en 100% */
 position:sticky;
 background-size:100% 100%;}
 
-
+.v-card--reveal {
+  bottom: 0;
+  opacity: 1 !important;
+  position: absolute;
+  width: 100%;
+}
 </style>
 
+<script>
+  export default {
+    data: () => ({
+      reveal: false,
+    }),
+  }
+</script>
