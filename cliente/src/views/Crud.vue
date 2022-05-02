947ee8bcaa86e4f73fd4a5d38c5b0201648dfbb9
Emilio Silva <esilva@tibox.cl>
2022-05-02 05:19:03 -0400

    hotfix: errores crud arreglados, aportes flat obsoletos quitados, console.log borrados

--- cliente/src/views/Crud.vue
+++ cliente/src/views/Crud.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
-        <v-bottom-navigation
-            v-model="value"
-            shift
-        >
-            <v-btn  @click="opcionBoton(1)">Mascotas</v-btn>
-            <v-btn  @click="opcionBoton(0)">Administradores</v-btn>
-            <v-btn  @click="opcionBoton(2)">Formularios</v-btn>
-            <v-btn  @click="opcionBoton(3)">Socios</v-btn>
-        </v-bottom-navigation>
+    <v-bottom-navigation v-model="value" shift>
+      <v-btn @click="opcionBoton(0)">Mascotas</v-btn>
+      <v-btn @click="opcionBoton(1)">Administradores</v-btn>
+      <v-btn @click="opcionBoton(2)">Formularios</v-btn>
+      <v-btn @click="opcionBoton(3)">Socios</v-btn>
+    </v-bottom-navigation>
     >
-    <crudAdmin v-if="opcion == 0"></crudAdmin>
-    <crudMascotas v-if="opcion == 1"></crudMascotas>
-    <crud-form v-if="opcion==2"></crud-form>
+    <crudMascotas v-if="opcion == 0"></crudMascotas>
+    <crudAdmin v-if="opcion == 1"></crudAdmin>
+    <crud-form v-if="opcion == 2"></crud-form>
     <crudSocios v-if="opcion == 3"></crudSocios>
   </div>
 </template>
@@ -20,29 +17,27 @@
 <script>
 import crudAdmin from "../components/crudA.vue";
 import crudMascotas from "../components/crudM.vue";
-import CrudForm from '../components/crudForm.vue';
+import CrudForm from "../components/crudForm.vue";
 import crudSocios from "./Socios.vue";
 
 export default {
   data() {
     return {
-            opcion : 1,
-            value: 0
+      opcion: 0,
+      value: 0,
+    };
   },
   name: "Crud",
   components: {
     crudAdmin,
     crudMascotas,
-    CrudForm
+    CrudForm,
     crudSocios,
   },
   methods: {
-        opcionBoton(opcion){
-            this.opcion = opcion
+    opcionBoton(opcion) {
+      this.opcion = opcion;
     },
+  },
+};
 </script>
-
-<style>
-
-</style>
-</style>
