90281923f91d647cc56bf014c480ea95a9239678
ChristianLizama <ckill661@gmail.com>
2022-05-08 02:41:39 -0400

    Eliminación de un caracter sobrante

--- cliente/src/views/Crud.vue
+++ cliente/src/views/Crud.vue
@@ -6,7 +6,6 @@
       <v-btn @click="opcionBoton(2)">Formularios</v-btn>
       <v-btn @click="opcionBoton(3)">Socios</v-btn>
     </v-bottom-navigation>
-    >
     <crudMascotas v-if="opcion == 0"></crudMascotas>
     <crudAdmin v-if="opcion == 1"></crudAdmin>
     <crud-form v-if="opcion == 2"></crud-form>
