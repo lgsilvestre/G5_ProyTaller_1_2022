0aec588c0e143c049d2eef0cce9a27069d0829d6
Emilio Silva <esilva@tibox.cl>
2022-05-08 23:10:20 -0400

    filtro mas retoques diseño catalogo de animales

--- cliente/src/components/catalogItem.vue
+++ cliente/src/components/catalogItem.vue
@@ -1,41 +1,26 @@
 <template>
-  <v-card
-    class="pa-2"
-    max-width="450px"
-    style="margin: 0 auto"
-  >
-    <v-img
-      :src="item.foto"
-      height="200px"
-    ></v-img>
+  <v-card class="pa-2 cardItem" shaped max-width="450px" style="margin: 0 auto">
+    <v-img :src="item.foto" height="200px"></v-img>
 
     <v-card-title>
-      {{item.nombre}}
+      {{ item.nombre }}
     </v-card-title>
 
     <v-card-subtitle>
-        <ul>
-            <li>{{item.raza}}</li>
-            <li>{{item.tipo}}</li>
-            <li>{{item.edad}} años</li>
-        </ul>
+      <ul>
+        <li>{{ item.raza }}</li>
+        <li>{{ item.tipo }}</li>
+        <li>{{ item.edad }} años</li>
+      </ul>
     </v-card-subtitle>
 
     <v-card-actions>
-      <v-btn
-        color="red lighten-2"
-        text
-      >
-        Adoptame
-      </v-btn>
+      <v-btn color="red lighten-2" text> Adoptame </v-btn>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        icon
-        @click="show = !show"
-      >
-        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      <v-btn icon @click="show = !show">
+        <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
       </v-btn>
     </v-card-actions>
 
@@ -43,7 +28,12 @@
       <div v-show="show">
         <v-divider></v-divider>
 
-        <v-card-text> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit officia quibusdam quod doloremque necessitatibus neque totam voluptatem architecto quas cum eius, dicta optio. Ut voluptates corporis maiores nisi unde aliquid? </v-card-text>
+        <v-card-text>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit
+          officia quibusdam quod doloremque necessitatibus neque totam
+          voluptatem architecto quas cum eius, dicta optio. Ut voluptates
+          corporis maiores nisi unde aliquid?
+        </v-card-text>
       </div>
     </v-expand-transition>
   </v-card>
@@ -51,9 +41,15 @@
 
 <script>
 export default {
-    props: ['item'],
-    data: () => ({
-        show: false,
-    }),
-}
+  props: ["item"],
+  data: () => ({
+    show: false,
+  }),
+};
 </script>
+
+<style>
+.cardItem {
+  border: 1px solid #ccc;
+}
+</style>
