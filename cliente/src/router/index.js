f3117fb6e778bbc73b3d68612c4cc4e9d07a6ba0
Sebastian <sebastian.galdames55@gmail.com>
2022-06-28 02:50:38 -0400

    primera prueba contacto

--- cliente/src/router/index.js
+++ cliente/src/router/index.js
@@ -37,12 +37,12 @@ const routes = [
     component: () => import(/* webpackChunkName: "eventos" */ '../views/Eventos.vue')
   },
   {
-    path: '/aportes',
-    name: 'Aportes',
+    path: '/contacto',
+    name: 'Contacto',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "aportes" */ '../views/Aportes.vue')
+    component: () => import(/* webpackChunkName: "eventos" */ '../views/Eventos.vue')
   },
   {
     path: '/tenenciaResponsable',
