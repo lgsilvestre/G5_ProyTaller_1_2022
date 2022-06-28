d6e9699b25675b39cfcd67519fc987763e7283eb
Sebastian <sebastian.galdames55@gmail.com>
2022-06-28 12:50:23 -0400

    fix aportes

--- cliente/src/router/index.js
+++ cliente/src/router/index.js
@@ -45,6 +45,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "eventos" */ '../views/Contact.vue')
   },
   {
+    path: '/aportes',
+    name: 'Aportes',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "eventos" */ '../views/Aportes.vue')
+  },
+  {
     path: '/tenenciaResponsable',
     name: 'TenenciaResponsable',
     // route level code-splitting
