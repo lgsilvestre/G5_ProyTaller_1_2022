6b8c777a0b29cae4e06739570da46018be10fdac
Sebastian <sebastian.galdames55@gmail.com>
2022-07-03 21:35:13 -0400

    mapa con veterinarios (Marcelo)

--- cliente/src/components/Contacto/contactForm.vue
+++ cliente/src/components/Contacto/contactForm.vue
@@ -1,20 +1,20 @@
 <template>
   <v-form>
     <v-row class="mb-n6">
-      <v-col><v-text-field label="Name" dense outlined></v-text-field></v-col>
+      <v-col><v-text-field label="Nombre" dense outlined></v-text-field></v-col>
       <v-col><v-text-field label="Email" dense outlined></v-text-field></v-col>
     </v-row>
 
     <v-row class="mb-n6">
       <v-col
-        ><v-text-field label="Subject" dense outlined></v-text-field
+        ><v-text-field label="Titulo" dense outlined></v-text-field
       ></v-col>
     </v-row>
     <v-row class="mb-n6">
       <v-col>
         <v-textarea
           dense
-          label="Your Message"
+          label="Mensaje"
           auto-grow
           outlined
           rows="8"
@@ -24,8 +24,8 @@
     </v-row>
     <v-row>
       <v-col
-        ><v-btn class="my-2" color="primary">SEND MESSAGE</v-btn
-        ><v-btn class="my-2 ml-3">clear</v-btn></v-col
+        ><v-btn class="my-2" color="primary">Enviar</v-btn
+        ><v-btn class="my-2 ml-3">Limpiar</v-btn></v-col
       >
     </v-row>
   </v-form>
