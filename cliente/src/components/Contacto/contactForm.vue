8c0ee0a55bc4b940c72f99861a4c1067bcc0bf63
Sebastian <sebastian.galdames55@gmail.com>
2022-07-03 22:24:39 -0400

    envios de correo y mejoras home

--- cliente/src/components/Contacto/contactForm.vue
+++ cliente/src/components/Contacto/contactForm.vue
@@ -1,32 +1,35 @@
 <template>
-  <v-form>
+  <v-form action="https://formsubmit.co/fundacion.adogtame@gmail.com" method="POST">
     <v-row class="mb-n6">
-      <v-col><v-text-field label="Nombre" dense outlined></v-text-field></v-col>
-      <v-col><v-text-field label="Email" dense outlined></v-text-field></v-col>
+      <v-col><v-text-field label="Nombre" name="nombre" dense outlined></v-text-field></v-col>
+      <v-col><v-text-field label="Email" name="email" dense outlined></v-text-field></v-col>
     </v-row>
 
     <v-row class="mb-n6">
       <v-col
-        ><v-text-field label="Titulo" dense outlined></v-text-field
+        ><v-text-field label="Asunto" name="Asunto" dense outlined></v-text-field
       ></v-col>
     </v-row>
     <v-row class="mb-n6">
       <v-col>
-        <v-textarea
+        <v-textarea 
+          name="mensaje"
           dense
           label="Mensaje"
           auto-grow
           outlined
           rows="8"
           row-height="20"
+          :rules="['Required']"
         ></v-textarea>
       </v-col>
     </v-row>
     <v-row>
-      <v-col
-        ><v-btn class="my-2" color="primary">Enviar</v-btn
-        ><v-btn class="my-2 ml-3">Limpiar</v-btn></v-col
-      >
+      <v-col>
+        <v-btn type="submit" value= "" class="my-3 red darken-3" >Enviar</v-btn>
+        <v-btn class="my-2 ml-3">Limpiar</v-btn>
+        <input type="hidden" name="_next" value="https://adogtamecurico.com/home">
+      </v-col>
     </v-row>
   </v-form>
 </template>
